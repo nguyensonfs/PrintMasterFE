7136a50a403f7be6b60c5c6d4f30117a8f7fd776
nguyensonfs <wingc.dev@gmail.com>
2024-08-26 09:48:31 +0700

    🚧 create project translation project status

---
+++ src/utils/translations.js
@@ -0,0 +1,7 @@
+export const projectStatusTranslations = {
+  Initialization: 'Đang trong thời gian hoàn thành',
+  'In Progress': 'Đang tiến hành',
+  Completed: 'Hoàn thành',
+  'On Hold': 'Tạm dừng',
+  Cancelled: 'Hủy bỏ',
+}
