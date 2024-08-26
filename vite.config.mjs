7f75f18ac7ede21b77d64ff98c2d4ecb3f946d6b
nguyensonfs <wingc.dev@gmail.com>
2024-08-26 09:17:57 +0700

    🔧 config ant design

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,14 @@
 // Plugins
-import Components from 'unplugin-vue-components/vite'
 import Vue from '@vitejs/plugin-vue'
-import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import AutoImport from 'unplugin-auto-import/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import fontLoader from './src/plugins/fontLoader'
 
 // Utilities
-import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
@@ -29,6 +30,11 @@ export default defineConfig({
       dirs: ['src/components', 'src/stores'],
       extensions: ['vue', 'js'],
       deep: true,
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false,
+        }),
+      ],
     }),
 
     AutoImport({
