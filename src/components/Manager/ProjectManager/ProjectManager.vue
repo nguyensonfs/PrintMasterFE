7a67947c4ed8633e7769a7835f9057db45d95965
nguyensonfs <wingc.dev@gmail.com>
2024-08-26 11:54:02 +0700

    🚧 feat view project

--- src/components/Manager/ProjectManager/ProjectManager.vue
+++ src/components/Manager/ProjectManager/ProjectManager.vue
@@ -23,6 +23,7 @@
         <v-card-item>
           <p>Leader: {{ project.leader }}</p>
           <p class="tw-mt-1">Ngày tạo: {{ formatDate(project.startDate) }}</p>
+          <p class="tw-mt-1">{{ translateStatus(project.projectStatus) }}</p>
           <p class="tw-mt-1">
             Tiến độ:
             <span class="tw-text-green-600">{{ project.progress }}%</span>
@@ -41,81 +42,100 @@
             base-color="primary"
             icon="mdi-eye-outline"
             size="small"
+            @click="openDialog(project)"
           ></v-btn>
         </template>
       </v-card>
     </v-col>
   </v-row>
   <!-- Dynamic Dialog -->
-  <v-dialog v-model="dialogVisible" max-width="500px">
+  <v-dialog
+    v-model="dialogVisible"
+    transition="dialog-bottom-transition"
+    fullscreen
+  >
     <v-card>
-      <v-card-title>{{ dialogTitle }}</v-card-title>
-      <v-card-text>
-        <!-- Add/Edit Form -->
-        <v-form v-if="mode !== 'delete' && mode !== 'changeHead'" ref="form">
-          <v-text-field
-            v-model="formData.name"
-            label="Tên Phòng Ban"
-            required
-          ></v-text-field>
-          <v-text-field
-            v-model="formData.description"
-            label="Mô Tả"
-          ></v-text-field>
-          <v-select
-            v-model="formData.managerId"
-            :items="availableHeads"
-            item-value="id"
-            item-title="fullName"
-            label="Chọn Trưởng Phòng Mới"
-            required
-          ></v-select>
-        </v-form>
-
-        <!-- Change Head Form -->
-        <v-form v-if="mode === 'changeHead'" ref="form">
-          <v-select
-            v-model="formData.managerId"
-            :items="availableHeads"
-            item-value="id"
-            item-title="fullName"
-            label="Chọn Trưởng Phòng Mới"
-            required
-          ></v-select>
-        </v-form>
-
-        <!-- Delete Confirmation -->
-        <div v-if="mode === 'delete'">
-          Bạn có chắc chắn muốn xóa phòng ban "{{ formData.name }}" không?
-        </div>
-      </v-card-text>
-      <v-card-actions>
+      <v-toolbar :height="40">
+        <v-toolbar-title>{{ formData?.projectName }}</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn
-          color="primary"
-          v-if="mode === 'add' || mode === 'edit'"
-          @click="saveChanges"
-        >
-          {{ mode === 'add' ? 'Thêm' : 'Lưu' }}
-        </v-btn>
-        <v-btn
-          color="secondary"
-          v-if="mode === 'changeHead'"
-          @click="changeHead"
-          >Đổi</v-btn
-        >
-        <v-btn color="red" v-if="mode === 'delete'" @click="deleteDepartment"
-          >Xóa</v-btn
-        >
-        <v-btn @click="dialogVisible = false">Hủy</v-btn>
-      </v-card-actions>
+        <v-toolbar-items>
+          <v-btn icon="mdi-close" @click="dialogVisible = false"></v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <!-- Nội dung Dialog -->
+      <v-container>
+        <v-row class="tw-justify-center tw-items-center tw-h-[60px]">
+          <v-col cols="auto">
+            <div>
+              <v-img
+                width="30"
+                height="30"
+                src="../../../assets/icons/project.svg"
+                class="tw-mx-auto"
+              ></v-img>
+              <span>Dự án</span>
+            </div>
+          </v-col>
+          <v-icon icon="mdi-chevron-right"> </v-icon>
+          <v-col cols="auto">
+            <div>
+              <v-img
+                width="30"
+                height="30"
+                src="../../../assets/icons/design.svg"
+                class="tw-mx-auto"
+              ></v-img>
+              <span>Thiết kế</span>
+            </div>
+          </v-col>
+          <v-icon icon="mdi-chevron-right"> </v-icon>
+          <v-col cols="auto">
+            <div>
+              <v-img
+                width="30"
+                height="30"
+                src="../../../assets/icons/print.svg"
+                class="tw-mx-auto"
+              ></v-img>
+              <span>In ấn</span>
+            </div>
+          </v-col>
+          <v-icon icon="mdi-chevron-right"> </v-icon>
+          <v-col cols="auto">
+            <div>
+              <v-img
+                width="30"
+                height="30"
+                src="../../../assets/icons/delivery.svg"
+                class="tw-mx-auto"
+              ></v-img>
+              <span>Giao hàng</span>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-divider
+        :thickness="2"
+        class="border-opacity-75 tw-mt-3"
+        color="info"
+      ></v-divider>
+      <v-card-text>
+        <p><strong>Leader:</strong> {{ formData?.leader }}</p>
+        <p><strong>Ngày tạo:</strong> {{ formatDate(formData?.startDate) }}</p>
+        <p>
+          <strong>Trạng thái:</strong>
+          {{ translateStatus(formData?.projectStatus) }}
+        </p>
+        <p><strong>Tiến độ:</strong> {{ formData?.progress }}%</p>
+        <!-- Thêm các trường khác nếu cần -->
+      </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 <script setup>
-import * as userService from '@/apis/userServices'
 import { FILE_URL } from '@/constants'
 import { useProjectStore } from '@/stores/project'
+import { projectStatusTranslations } from '@/utils/translations'
 import '@vuepic/vue-datepicker/dist/main.css'
 import dayjs from 'dayjs'
 import { onMounted } from 'vue'
@@ -125,100 +145,25 @@ const projectStore = useProjectStore()
 
 // variable
 const projects = computed(() => projectStore.getProject)
-const mode = ref('')
-const leaders = ref([])
 const dialogVisible = ref(false)
+const formData = ref(null)
 
 // computed
-const dialogTitle = computed(() => {
-  switch (mode.value) {
-    case 'add':
-      return 'Thêm dự án'
-    case 'edit':
-      return 'Sửa dự án'
-    case 'delete':
-      return 'Xoá dự án'
-    default:
-      return ''
-  }
-})
-
-const fetchLeaders = async () => {
-  const res = await userService.getUserRoleLeaderAPI()
-  leaders.value = res
+const openDialog = async (item = null) => {
+  dialogVisible.value = true
+  formData.value = item
 }
 
 const formatDate = (date) => {
   return date ? dayjs(date).format('DD/MM/YYYY') : 'Không có ngày'
 }
 
-const saveChanges = async () => {
-  try {
-    if (mode.value === 'add') {
-      const payload = {
-        name: formData.value.name,
-        description: formData.value.description,
-        managerId: formData.value.managerId,
-      }
-      const response = await teamService.createTeamAPI(payload)
-      if (response.status === 200) {
-        toast.success(response.message)
-      }
-      departments.value.push(response.data)
-    } else if (mode.value === 'edit') {
-      const payload = {
-        name: formData.value.name,
-        description: formData.value.description,
-        managerId: formData.value.managerId,
-      }
-      const response = await teamService.updateTeamAPI(
-        formData.value.id,
-        payload
-      )
-      if (response.status === 200) {
-        toast.success(response.message)
-      }
-      const index = departments.value.findIndex(
-        (dep) => dep.id === formData.value.id
-      )
-      if (index !== -1) {
-        departments.value[index] = response.data
-      }
-    }
-    dialogVisible.value = false
-  } catch (error) {
-    console.error('Failed to save changes', error)
-  }
-}
-
-const openDialog = async (actionMode, item = null) => {
-  mode.value = actionMode
-
-  if (['addRole', 'delete', 'changeTeam'].includes(actionMode)) {
-    setFormData(item)
-  }
-  if (actionMode === 'addRole') {
-    await getRoles()
-    await getUserRoles(item.id)
-  }
-  dialogVisible.value = true
-}
-
-const setFormData = (item) => {
-  formData.value = {
-    id: item?.id || null,
-    fullName: item?.fullName || '',
-    email: item?.email || '',
-    phoneNumber: item?.phoneNumber || '',
-    avatar: item?.avatar || '',
-    teamName: item?.teamName || '',
-    teamId: item?.teamId || '',
-  }
+const translateStatus = (status) => {
+  return projectStatusTranslations[status] || status
 }
 
 // onMounted
 onMounted(() => {
   projectStore.fetchAllProject()
-  fetchLeaders()
 })
 </script>
